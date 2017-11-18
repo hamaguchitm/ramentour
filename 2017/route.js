3a24b51c279ac8bb895dfbc6146aeabdc888ffa9
Tomohiro Hamaguchi <tomohiro.hamaguchi@gmail.com>
2017-11-19 00:22:23 +0900

    modify location icon

--- 2017/route.js
+++ 2017/route.js
@@ -2,48 +2,26 @@ var map;
 var layer;
 var layerOptions;
 var kmlSrc = 'https://hamaguchitm.github.io/ramentour/2017/kml/route.kml'
+var iconImage = 'https://hamaguchitm.github.io/ramentour/2017/img/bluedot.png'
 var myLatLng
-
-function getLocation(){
-    if (navigator.geolocation) {
-        myLatLng = navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-    } else {
-        alert("本ブラウザではGeolocationが使えません");
-    }
-}
-function successCallback(pos) {
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        icon: {
-            url: 'img/bluedot.png',
-            size: new google.maps.Size(100, 100)
-        },
-        map: map
-    });
-}
-function errorCallback(error) {
-    alert( "位置情報が許可されていません");
-    myLatLng = null;
-}
-
 
 function initMap() {
     var center = new google.maps.LatLng(35.675581, 139.692387);
 
     map = new google.maps.Map(document.getElementById('map'), {
         center: center,
-        zoom: 12,
+        zoom: 13,
         mapTypeId: google.maps.MapTypeId.TERRAIN
     });
 
-    // Route
+    // ルート表示
     var kmlLayer = new google.maps.KmlLayer(kmlSrc, {
         suppressInfoWindows: true,
         preserveViewport: false,
         map: map
     });
 
-    // Shop
+    // ラーメン屋表示
     var shopLayer = new google.maps.FusionTablesLayer({
         query: {
             select: 'latitude',
@@ -57,6 +35,24 @@ function initMap() {
         map: map
     })
 
-    getLocation()
+    // 現在地表示
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            function(position) {
+                myLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                var marker = new google.maps.Marker({
+                   position: myLatLng,
+                   icon: iconImage,
+                   map: map
+               });
+            },
+            function(error) {
+                alert( "位置情報が許可されていません");
+                myLatLng = null;
+            }
+        );
+    } else {
+        alert("本ブラウザではGeolocationが使えません");
+    }
 }
 google.maps.event.addDomListener(window, 'load', initMap);
